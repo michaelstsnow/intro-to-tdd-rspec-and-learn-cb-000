333573de5dd018bb86d83c3744525ee99c5ce029
michaelstsnow <michael.st.snow@gmail.com>
2018-05-18 00:31:55 +0000

    automatically backed up by learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,6 +1,5 @@
 #First I want to define the methond
 def current_age_for_birth_year(x)
   #First get the current year
-  current_year= Time.now.year
-  current_age=current_year-x
+  current_age=2003-x
 end
